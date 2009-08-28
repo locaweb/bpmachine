d2621a1e6a634d6a5bb14ae4d13af037571b788e
fabiokung <fabio.kung@gmail.com>
2009-08-27 21:52:09 -0300

    high level spec

--- spec/bpmachine_spec.rb
+++ spec/bpmachine_spec.rb
@@ -1,7 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Bpmachine" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Business Process execution engine" do
+  it "should provide a DSL to describe processes" do
+    
+    class Machine
+      include ProcessSpecification
+      
+      process :of => :uninstall do
+        must_be :deactivated
+        
+        transition :remove_disks, 
+          :from => :deactivated, 
+          :to => :diskless
+        
+        transition :destroy_vm, 
+          :from => :diskless, 
+          :to => :vm_destroyed
+        
+        transition :erase_data, 
+          :from => :vm_destroyed, 
+          :to => :uninstalled
+      end      
+    end
+    
+    machine = Machine.new
+    machine.should_receive :remove_disks
+    machine.should_receive :destroy_vm
+    machine.should_receive :erase_data
+    machine.uninstall
+    
+    machine.status.should be(:uninstalled)
   end
+  
 end
